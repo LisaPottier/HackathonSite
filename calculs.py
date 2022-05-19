5eeec3106ed70f8576b11bf50511764e7ba905ed
Lisa <Lisa@pottier.eu>
2022-05-19 17:54:13 +0200

    [createDistanceCsv] calculeDistance OK

--- calculs.py
+++ calculs.py
@@ -3,10 +3,8 @@ import math
 #source : https://janakiev.com/blog/gps-points-distance-python/ 
 
 #calcul de la distance entre deux points GPS
-def haversine(coord1, coord2):
+def haversine(lat1, lon1, lat2, lon2):
     R = 6372800  # Rayon de la Terre (en mètre)
-    lat1, lon1 = coord1
-    lat2, lon2 = coord2
     
     phi1, phi2 = math.radians(lat1), math.radians(lat2) 
     dphi       = math.radians(lat2 - lat1)
--- createDistanceCsv.py
+++ createDistanceCsv.py
@@ -1,1 +1,22 @@
-import haversine from 
+from calculs import haversine 
+import os, sys, csv
+import string
+
+
+def distance_min(struct_lat, struct_lon):
+    distance_min=10000
+
+    with open('coordonnees-structures.csv', newline='') as csvfile:
+        pointsTerritoire = csv.DictReader(csvfile)
+        for point in pointsTerritoire:
+            if(point['geocodage']!= ''):
+                current_coord = point['geocodage'].partition(',')
+                current_lat = float(current_coord[0])
+                current_lon = float(current_coord[2])
+                distance= haversine(current_lat, current_lon, struct_lat, struct_lon)
+                if(distance < distance_min):
+                    distance_min=distance
+    return distance_min
+
+print(distance_min(48.579933,7.756877))
+#'coordonnees-des-structures-dgfip.csv'
